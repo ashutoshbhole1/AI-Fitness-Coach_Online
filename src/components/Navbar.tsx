f86ccc9eb8776693477cc8431f2ec479534c9ecb
Ashutosh Bhole <Ashutoshbhole1@gmail.com>
2025-09-22 10:09:42 +0530

    sdf

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,29 @@
+"use client "
+import Link from 'next/link'                   
+import { Link as LinkIcon, ZapIcon } from 'lucide-react' 
+
+import React from 'react'
+
+const Navbar = () => {
+    // const {isSignedIn} = useUser();
+
+    return (
+        <header className='fixed top-0 left-0 right-0 z-50 bg-background/60 backdrop-blur-md  border-b border-border py-3'>
+            <div className="container mx-auto  flex items-center justify-between">
+
+                <Link href='/' className='flex items-center gap-2'>
+                    <div className="p-1 bg-primary/10 rounded">
+
+                        <ZapIcon className='w-4 h-4 text-primary' />
+                    </div>
+                    <span className="text-xl font-bold font-mono">
+                        AI <span className="text-primary">Fitness</span> Coach
+                    </span>
+                </Link>
+            </div>
+
+        </header>
+    )
+}
+
+export default Navbar
