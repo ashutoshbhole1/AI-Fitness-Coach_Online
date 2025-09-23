ff12f1f91d2e8586783cafea85a5715db9723521
Ashutosh Bhole <Ashutoshbhole1@gmail.com>
2025-09-23 13:07:45 +0530

    Hero Section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,163 @@
-"use client"
-import { SignedIn, SignedOut, SignIn, SignOutButton } from '@clerk/nextjs'
-import React from 'react'
+import { ArrowRight } from "lucide-react"
+import { Button } from "@/components/ui/button"
+import Link from "next/link"
+import TerminalOverlay from "@/components/TerminalOverlay"
+import UserProgram from "@/components/UserProgram"
 
-const page = () => {
+const HomePage = () => {
   return (
-    <div>
-      <h1>Jai shree krishna</h1>
-      This is HOME va page
-<br />
-      <SignedOut>
-        <SignIn />
-      </SignedOut>
-<br />
-      <SignedIn>
-        <SignOutButton />
-      </SignedIn>
-    </div>
+    <div className="flex flex-col min-h-screen text-foreground overflow-hidden">
+      <section className="relative z-10 py-10 flex-grow">
+{/* Hero section */}
+        <div className="container mx-auto px-4">
+          <div className="grid grid-cols-1 lg:grid-cols-12 gap-8 items-center relative">
+
+            {/* Corner Decoration */}
+            <div className="absolute -top-10 left-0 w-40 h-40 border-l-2 border-t-2" />
+
+            {/* Left Side section */}
+            <div className="lg:col-span-7 space-y-8 relative">
+              <h1 className="text-5xl md:text-6xl lg:text-7xl font-bold tracking-tight">
+                <div><span className="text-foreground">Transform</span></div>
+                <div><span className="text-primary">Your Body</span></div>
+                <div className="pt-2"><span className="text-foreground">With Advanced</span></div>
+                <div className="pt-2"><span className="text-foreground">AI</span> <span className="text-primary">Technology</span></div>
+              </h1>
+
+
+              {/* Seperator Line */}
+              <div className="h-px w-full bg-gradient-to-r from-primary via-secondary to-primary opacity-50"></div>
+
+              <p className="text-xl text-muted-foreground w-2/3">
+                Talk to our AI Coach and get personalised diet plans and workout routines designed only for you.
+              </p>
+
+
+              {/* stats */}
+              <div className="flex items-center gap-10 py-6 font-mono">
+
+                <div className="flex flex-col">
+                  <div className="text-2xl text-primary">500+</div>
+                  <div className="text-xs uppercase tracking-wider">ACTIVE USERS</div>
+                </div>
+
+                <div className="h-12 w-px bg-gradient-to-b from-transparent via-border to-transparent"></div>
+                <div className="flex flex-col">
+                  <div className="text-2xl text-primary">3 min</div>
+                  <div className="text-xs uppercase tracking-wider">GENERATION</div>
+                </div>
+
+                <div className="h-12 w-px bg-gradient-to-b from-transparent via-border to-transparent"></div>
+
+                <div className="flex flex-col">
+                  <div className="text-2xl text-primary">100%</div>
+                  <div className="text-xs uppercase tracking-wider">PERSONALIZED</div>
+                </div>
+              </div>
+
+              {/* Button */}
+              <div className="flex flex-col sm:flex-row gap-4 pt-6">
+                <Button
+                  size="lg"
+                  asChild
+                  className="overflow-hidden bg-primary text-primary-foreground px-8 py-6 text-lg font-medium">
+                  <Link href={"/generate-program"} className="flex items-center font-mono">
+                    Build Your Program
+                    <ArrowRight className="ml-2 size-5" />
+                  </Link>
+
+                </Button>
+              </div>
+
+            </div>
+
+
+            {/* Right Side section */}
+            <div className="lg:col-span-5 relative">
+
+              {/* corners */}
+              <div className="absolute -inset-4 pointer-events-none">
+                <div className="absolute top-0 left-0 h-16 w-16 border-l-2 border-t-2 border-border "></div>
+                <div className="absolute top-0 right-0 h-16 w-16 border-r-2 border-t-2 border-border "></div>
+                <div className="absolute bottom-0 left-0 h-16 w-16 border-l-2 border-b-2 border-border "></div>
+                <div className="absolute bottom-0 right-0 h-16 w-16 border-r-2 border-b-2 border-border "></div>
+              </div>
+
+              {/* IMAGE CONTAINER */}
+              <div className="relative aspect-square max-w-lg mx-auto">
+                <div className="relateve overflow-hidden rounded-lg bg-cyber-black">
+                  <img src="/hero-ai5.png" alt="AI FITNESS COACH" className="w-full object-cover object-center" />
+                </div>
+
+
+                {/* SCAN LINE */}
+                <div className="absolute inset-0 bg-[linear-gradient(transparent_0%,transparent_calc(50%-1px),var(--cyber-glow-primary)_50%,transparent_calc(50%+1px),transparent_100%)] bg-[length:100%_8px] animate-scanline pointer-events-none" />
+
+                {/* DECORATION ON TOP OF IMAGE */}
+                <div className="absolute inset-0 pointer-events-none">
+                  <div className="absolute top-1/3 left-1/3 w-1/3 h-1/3 border border-primary/40 rounded-full " />
+                  {/* Targeting Lines */}
+                  <div className="absolute top-1/2 left-0 w-1/4 h-px bg-primary/50"></div>
+                  <div className="absolute top-1/2 right-0 w-1/4 h-px bg-primary/50"></div>
+
+                  <div className="absolute top-0 left-1/2 w-px h-1/4 bg-primary/50"></div>
+                  <div className="absolute bottom-0 left-1/2 w-px h-1/4 bg-primary/50"></div>
+
+
+
+
+                </div>
+
+                <div className="absolute inset-0 bg-gradient-to-t from-background via-background/40 to-transparent" />
+
+                {/* terminal overlay */}
+                <TerminalOverlay />
+              </div>
+
+
+            </div>
+
+
+
+
+
+          </div>
+        </div>
+      </section >
+
+
+      {/* UserProgram */}
+      
+      
+      <UserProgram/>
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+
+
+    </div >
   )
 }
 
-export default page
+export default HomePage
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,15 +1,24 @@
-"use client "
-import Link from 'next/link'                   
-import { Link as LinkIcon, ZapIcon } from 'lucide-react' 
+"use client"
+import Link from 'next/link';
 
+import {
+    Link as LinkIcon, ZapIcon, HomeIcon, UserIcon, DumbbellIcon,
+} from "lucide-react";
+
+
+import { Button, buttonVariants } from './ui/button';
 import React from 'react'
+import { UserButton, useUser } from "@clerk/nextjs";
+import { SignInButton, SignUpButton } from '@clerk/nextjs'
 
 const Navbar = () => {
-    // const {isSignedIn} = useUser();
+    const { isSignedIn } = useUser();
 
     return (
         <header className='fixed top-0 left-0 right-0 z-50 bg-background/60 backdrop-blur-md  border-b border-border py-3'>
             <div className="container mx-auto  flex items-center justify-between">
+
+                {/* Logo icon */}
 
                 <Link href='/' className='flex items-center gap-2'>
                     <div className="p-1 bg-primary/10 rounded">
@@ -17,9 +26,77 @@ const Navbar = () => {
                         <ZapIcon className='w-4 h-4 text-primary' />
                     </div>
                     <span className="text-xl font-bold font-mono">
-                        AI <span className="text-primary">Fitness</span> Coach
+                        AI<span className="text-primary">Fitness</span>Coach
                     </span>
                 </Link>
+
+                {/* Navigation */}
+                <nav className="flex items-center gap-5">
+                    {isSignedIn ? (
+                        <>
+                            <Link
+                                href="/"
+                                className='flex items-center gap-1.5 text-sm hover:text-primary transition-colors'>
+                                {/* <Home size={16} /> */}
+                                <HomeIcon size={16} />
+                                <span>Home</span>
+                            </Link>
+
+
+                            <Link
+                                href="/generate-program"
+                                className='flex items-center gap-1.5 text-sm hover:text-primary transition-colors'>
+                                {/* <Dumbble size={16} /> */}
+                                <DumbbellIcon size={16} />
+                                <span>Generate</span>
+                            </Link>
+
+                            <Link
+                                href="/profile"
+                                className='flex items-center gap-1.5 text-sm hover:text-primary transition-colors'>
+                                {/* <User size={16} /> */}
+                                <UserIcon size={16} />
+                                <span>Profile</span>
+                            </Link>
+
+
+                            <Button
+                                asChild
+                                variant="outline"
+                                className='ml-2 border-primary/50 text-primary hover:text-white hover:bg-primary/10'
+                            >
+                                <Link href='/generate-program'>Get Started</Link>
+
+                            </Button>
+
+                            <UserButton />
+                        </>
+                    ) : (
+                        <>
+
+                            <SignInButton>
+                                <Button
+                                    variant={'outline'}
+                                    className='border-primary/50 text-primary hover:text-white hover:bg-primary/10'>
+                                    Sign In
+                                </Button>
+                            </SignInButton>
+
+                            <SignUpButton>
+                                <Button
+                                    className='border-primary/50  hover:text-white hover:bg-primary/10'>
+                                    Sign Up
+                                </Button>
+                            </SignUpButton>
+
+
+
+
+                        </>
+                    )}
+                </nav>
+
+
             </div>
 
         </header>
